8dbc6e146ecf2cc72009cd04e1b15a472cace2d8
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2013-07-14 14:34:56 -0300

    Added logout link. Fixes #5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   respond_to :html, :json, :js
+  layout :layout
+
+  def layout
+    current_user ? 'application' : 'login'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Agenda::Application.routes.draw do
   devise_for :users
   resources :tasks
 
+  root to: 'tasks#index'
   put '/test' => 'users#test_action'
 end
